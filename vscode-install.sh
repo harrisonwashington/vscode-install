dade0fe872ed5b55571ee19fde2139690f1a6fa1
harrisonwashington <59775376+harrisonwashington@users.noreply.github.com>
2022-03-23 10:51:55 -0700

    vscode-install.sh
    Bash script for installing vscode on new machine

---
+++ vscode-install.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+# Update the package index and install the dependencies
+sudo apt update&&apt update
+sudo apt install software-properties-common apt-transport-https wget
+# Import the Microsoft GPG key
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+# Enable the Visual Studio Code repository
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+# Visual Studio Code Deb file install 
+wget https://code.visualstudio.com/docs/?dv=linux64_deb
+sudo apt update
+# Visual Studio Code apt install
+sudo apt install code
