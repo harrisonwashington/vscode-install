7a25f7234b80a13fa04b0df31ffd25ee83a475db
harrisonwashington <59775376+harrisonwashington@users.noreply.github.com>
2022-05-31 20:26:59 +0000

    worked on comments

--- vscode-install.sh
+++ vscode-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Update the package index and install the dependencies
+# Update package index and install the necessary dependencies
 sudo apt update && apt upgrade -y 
 sudo apt install software-properties-common apt-transport-https wget
 # Import the Microsoft GPG key
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 # Enable the Visual Studio Code repository
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-# Visual Studio Code Deb file install 
+# Visual Studio Code Deb file install with wget & package update 
 wget https://code.visualstudio.com/docs/?dv=linux64_deb
 sudo apt update
 # Visual Studio Code apt install
